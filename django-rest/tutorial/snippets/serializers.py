8f38a2bad21c568ee21d1fd2c93347d4e0d15ed5
Lan Chu <hoanglan.ict@gmail.com>
2023-05-11 13:33:07 +0700

    add tutorial

---
+++ django-rest/tutorial/snippets/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Snippet
+        fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
---
+++ django-rest/tutorial/snippets/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
