903f5331d5fae4d1ce55a9414e8806f963bcc301
Lan Chu <hoanglan.ict@gmail.com>
2023-05-30 14:06:07 +0700

    update pipeline

--- airflow/dags/tasks/extract_features.py
+++ airflow/dags/tasks/extract_features.py
@@ -1,25 +1,28 @@
 import json
 import os
+import socket
 import subprocess
 from types import SimpleNamespace
 
+import pandas as pd
 from airflow.decorators import task
 from joblib import load
-
-CUR_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 @task()
 def extract_features(json_comments):
-    print(CUR_DIR)
+    AIRFLOW_HOME = os.getenv("AIRFLOW_HOME")
+    base_file_path = AIRFLOW_HOME + "/dags/tasks/model"
+    vect_file_path = base_file_path + "/tfidf_vectorizer.joblib"
+    model_file_path = base_file_path + "/movie_review_sa_v1.joblib"
+    vectorizer = load(vect_file_path)
+    model = load(model_file_path)
+    comments = json.loads(json_comments, object_hook=lambda d: SimpleNamespace(**d))
 
-    result = subprocess.run(['ls'], stdout=subprocess.PIPE)
-    print(result.stdout)
+    for comment in comments:
+        feature_matrix = vectorizer.transform([comment.cleaned_content])
+        sentiment = model.predict(feature_matrix)[0]
+        comment.sentiment = sentiment
 
-    # vectorizer = load('f"{CUR_DIR}/model/tfidf_vectorizer.joblib"')
-    # comments = json.loads(
-    #     json_comments, object_hook=lambda d: SimpleNamespace(**d))
-    # for comment in comments:
-    #     comment.feature = vectorizer.transform([comment.cleaned_content])
-    # featured_json_comments = json.dumps([ob.__dict__ for ob in comments])
-    # return featured_json_comments
+    featured_json_comments = json.dumps([ob.__dict__ for ob in comments])
+    return featured_json_comments
